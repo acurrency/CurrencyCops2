63838533725899ccc1fa93814f2aabb9cda7866e
acurrency <christopher.phillips@currencycops.com>
2019-10-22 15:48:41 -0400

    l
    k

--- cart.php
+++ cart.php
@@ -45,37 +45,7 @@
     </section>
 
 
-    <?php
-
-    $dbname = 'currencycops';
-    $dbuser = 'ms7i273jabfw';
-    $dbpass = 'G3t2work!';
-    $dbhost = 'localhost';
     
-    $link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Unable to Connect to '$dbhost'");
-    mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
-    
-    
-$product_array = $db_handle->runQuery("SELECT * FROM tblproduct ORDER BY id ASC");
-if (!empty($product_array)) { 
-	foreach($product_array as $key=>$value){
-?>
-	<div class="product-item">
-		<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
-		<div class="product-img"><img src="<?php echo $product_array[$key]["img"]; ?>"></div>
-		<div class="product-tile-footer">
-		<div class="product-title"><?php echo $product_array[$key]["name"]; ?></div>
-		<div class="product-price"><?php echo "$".$product_array[$key]["price"]; ?></div>
-		<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
-		</div>
-		</form>
-	</div>
-<?php
-	}
-}
-?>
-
-
 
 
 
@@ -94,7 +64,7 @@ if (!empty($product_array)) {
     <!-- End Banner Area -->
 
     <!--================Cart Area =================-->
-    <!--<section class="cart_area">
+    <section class="cart_area">
         <div class="container">
             <div class="cart_inner">
                 <div class="table-responsive">
@@ -281,7 +251,7 @@ if (!empty($product_array)) {
             </div>
         </div>
     </section>
--->
+
     <!--================End Cart Area =================-->
     <?php include 'footer.php';?>
 
