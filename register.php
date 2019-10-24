5ae4743fa761218bab1f7d3af759df84d5d7570f
acurrency <christopher.phillips@currencycops.com>
2019-10-24 15:18:34 -0400

    w
    w

--- register.php
+++ register.php
@@ -72,12 +72,11 @@
       <input type="password" placeholder="Retype Password" name="psw-repeat" required>
       <hr>
   
-      <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
       <button type="submit" class="registerbtn">Register</button>
     </div>
   
     <div class="container signin">
-      <p>Already have an account? <a href="#">Sign in</a>.</p>
+      <p>Already have an account? <a href="login.php">Sign in</a>.</p>
     </div>
   </form> 
 
