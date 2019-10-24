791639b48d2b240ea7b1b395e0606a483f8241d9
acurrency <christopher.phillips@currencycops.com>
2019-10-24 13:42:20 -0400

    about page
    implementation

---
+++ about.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+        <!-- Mobile Specific Meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Favicon-->
+        <link rel="shortcut icon" href="img/fav.png">
+        <!-- Author Meta -->
+        <meta name="author" content="CodePixar">
+        <!-- Meta Description -->
+        <meta name="description" content="">
+        <!-- Meta Keyword -->
+        <meta name="keywords" content="">
+        <!-- meta character set -->
+        <meta charset="UTF-8">
+        <!-- Site Title -->
+        <title>CurrencyCops</title>
+        <link href = "img/product/llogo.png" rel = "icon" type = "image/x-icon">
+
+        <!--
+            CSS
+            ============================================= -->
+        <link rel="stylesheet" href="css/linearicons.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/themify-icons.css">
+        <link rel="stylesheet" href="css/nice-select.css">
+        <link rel="stylesheet" href="css/nouislider.min.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body id="category">
+
+<?php include 'header.php';?>
+
+<section class="banner-area organic-breadcrumb">
+                <div class="container">
+                        
+                </div>
+
+
+<?php include 'footer.php';?>
