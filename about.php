d81b64d4c9a22911b24f5919352c81545dfc7507
acurrency <christopher.phillips@currencycops.com>
2019-10-24 16:38:13 -0400

    de
    de

--- about.php
+++ about.php
@@ -1,149 +1,159 @@
-<!DOCTYPE html>
-<html lang="zxx" class="no-js">
 
-<head>
-	<!-- Mobile Specific Meta -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<!-- Favicon-->
-	<link rel="shortcut icon" href="img/fav.png">
-	<!-- Author Meta -->
-	<meta name="author" content="CodePixar">
-	<!-- Meta Description -->
-	<meta name="description" content="">
-	<!-- Meta Keyword -->
-	<meta name="keywords" content="">
-	<!-- meta character set -->
-	<meta charset="UTF-8">
-	<!-- Site Title -->
-	<title>CurrencyCops</title>
-    <link href = "img/product/llogo.png" rel = "icon" type = "image/x-icon">
-    <link href = "about.css" rel = "stylesheet">
+<!doctype html>
+<html lang="en-us">
 
+    <head>
 
-	<!--
-		CSS
-		============================================= -->
-	<link rel="stylesheet" href="css/linearicons.css">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/themify-icons.css">
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/owl.carousel.css">
-	<link rel="stylesheet" href="css/nice-select.css">
-	<link rel="stylesheet" href="css/nouislider.min.css">
-	<link rel="stylesheet" href="css/ion.rangeSlider.css" />
-	<link rel="stylesheet" href="css/ion.rangeSlider.skinFlat.css" />
-	<link rel="stylesheet" href="css/magnific-popup.css">
-	<link rel="stylesheet" href="css/main.css">
-</head>
+        <!-- OPL Demos Google Analytics -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-3920997-4', 'auto');
+            ga('send', 'pageview');
+        </script>
 
-<body>
-<div id = "wrapper"> 
-	<!-- Start Header Area -->
-	<header class="header_area sticky-header">
-            <div class="main_menu">
-                <nav class="navbar navbar-expand-lg navbar-light main_box">
-                    <div class="container">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-                            <ul class="nav navbar-nav menu_nav ml-auto">
-                                <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
-                                <li class="nav-item"><a class="nav-link" href="category.php">Shop Inventory</a></li>
-                                <li class="nav-item"><a class="nav-link" href="linkup.php">Link-Up</a></li>
-                                <li class="nav-item"><a class="nav-link" href="index.html">Fresh Styles</a></li>
-                                <li class="nav-item"><a class="nav-link" href="about.php">About Us</a></li>
-                                <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+        <!-- Meta -->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
 
-                            </ul>
-                            <ul class="nav navbar-nav navbar-right">
-                                    <li class="nav-item"><a href="viewCart.php" class="cart"><span class="ti-bag"></span></a></li>
-                                    <li class="nav-item">
-                                        <button class="search"><span class="lnr lnr-magnifier" id="search"></span></button>
-                                    </li>
-                                </ul>	
-                    </div>
-                </nav>
-            </div>
-            <div class="search_input" id="search_input_box">
-                <div class="container">
-                    <form class="d-flex justify-content-between">
-                        <input type="text" class="form-control" id="search_input" placeholder="Search Here">
-                        <button type="submit" class="btn"></button>
-                        <span class="lnr lnr-cross" id="close_search" title="Close Search"></span>
-                    </form>
-                </div>
-            </div>
-        </header>
-        <!-- End Header Area -->
-        <section class="banner-area organic-breadcrumb">
+        <title>Currency Cops</title>
+        <meta name="description" content="">
+        <meta name="robots" content="noindex, nofollow" />
+
+        <!-- The compiled CSS file -->
+        <link rel="stylesheet" href="css/production.css">
+
+        <!-- Web fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+
+        <!-- favicon.ico. Place these in the root directory. -->
+        <link rel="shortcut icon" href="favicon.ico">
+
+    </head>
+
+    <body>
+    <?php include 'header.php';?>
+    <section class="banner-area organic-breadcrumb">
                 <div class="container">
                         
                 </div>
-        <main>
-<h2>ITEC Courses Taken Thus Far</h2>
-<ol>
-	<li>Intro to Computing ITEC 1001</li>
-	<li>Programming Fundamentals ITEC 2140</li>
-    <li>Intermediate Programming ITEC 2150</li>
-    <li>Intro to Info Systems ITEC 2201</li>
-    <li>Intro to Networks ITEC 3100</li>
-    <li>Advanced Programming ITEC 3150</li>
-    <li>Intro to Databases ITEC 3200</li>
-    <li>Information Security ITEC 3300</li>
+    <!-- Header -->
+    <header class="pt2 pb1 align--center">
+        <div class="container">
+            <p>Welcome to</p>
+            <h1 title="Camp Chippewa"><img class="logo" src="./img/logo.svg" alt="Camp Chippewa"></h1>
+        </div>
+    </header>
 
-</ol>
+    <!-- Body -->
+    <main>
+        
+        <div class="container">
 
-<h2>Current ITEC Course Fall 2019</h2>
-<ul>
-<li>Web Technologies</li>
-<li>Operating Systems</li>
-<li>Info Tech Project I</li>
-</ul>
+            <!-- Info -->
+            <section class="grid-row grid-row--center">
+                <!-- Cabin -->
+                <div class="grid-column span-half pt3 pb3 mobile-m order-1">
+                    <div class="relative">
+                        <img class="info-image relative z2" src="./img/cabin.jpg" alt="Cabin">
+                        <div class="pattern pattern--left-down absolute z1"></div>
+                    </div>
+                </div>
+                <div class="grid-column span-half pt3 pb3 mobile-m order-2">
+                    <h3>Cabin</h3>
+                    <p>Illa argumenta propria videamus, cur omnia sint paria peccata. Quamquam te quidem video minime esse deterritum. Beatus sibi videtur esse moriens. In qua quid est boni praeter summam&nbsp;voluptatem.</p>
+                </div>
+                <!-- Teepees -->
+                <div class="grid-column span-half pt3 pb3 mobile-m order-4">
+                    <h3>Teepees</h3>
+                    <p>A quibus propter discendi cupiditatem videmus ultimas terras esse peragratas. Laelius clamores sofòw ille so lebat Edere compellans gumias ex ordine&nbsp;nostros.</p>
+                </div>
+                <div class="grid-column span-half pt3 pb3 mobile-m order-3">
+                    <div class="relative">
+                        <img class="info-image relative z2" src="./img/teepee.jpg" alt="Teepee">
+                        <div class="pattern pattern--right-middle absolute z1"></div>
+                    </div>
+                </div>
+                <!-- Fishing -->
+                <div class="grid-column span-half pt3 pb3 mobile-m order-5">
+                    <div class="relative">
+                        <img class="info-image relative z2" src="./img/fishing.jpg" alt="Fishing">
+                        <div class="pattern pattern--left-up absolute z1"></div>
+                    </div>
+                </div>
+                <div class="grid-column span-half pt3 pb3 mobile-m order-6">
+                    <h3>Fishing</h3>
+                    <p>Que Manilium, ab iisque M. Intellegi quidem, ut propter aliam quampiam rem, verbi gratia propter voluptatem, nos amemus; An eum locum libenter invisit&nbsp;ultimas.</p>
+                </div>
+                <!-- Archery -->
+                <div class="grid-column span-half pt3 pb3 mobile-m order-8">
+                    <h3>Archery</h3>
+                    <p>At ille pellit, qui permulcet sensum voluptate. Quid in isto egregio tuo officio et tanta fide-sic enim existimo-ad corpus refers? Nec vero sum nescius esse utilitatem in&nbsp;historia.</p>
+                </div>
+                <div class="grid-column span-half pt3 pb3 mobile-m order-7">
+                    <div class="relative">
+                        <img class="info-image relative z2" src="./img/archery.jpg" alt="Archery">
+                        <div class="pattern pattern--right-down absolute z1"></div>
+                    </div>
+                </div>
+                <!-- Canoeing -->
+                <div class="grid-column span-half pt3 pb3 mobile-m order-9">
+                    <div class="relative">
+                        <img class="info-image relative z2" src="./img/canoeing.jpg" alt="Canoeing">
+                        <div class="pattern pattern--left-middle absolute z1"></div>
+                    </div>
+                </div>
+                <div class="grid-column span-half pt3 pb3 mobile-m order-10">
+                    <h3>Canoeing</h3>
+                    <p>Nam si propter voluptatem, quae est ista laus, quae possit e macello peti? Sin te auctoritas commovebat, nobisne omnibus et Platoni ipsi nescio quem illum&nbsp;anteponebas.</p>
+                </div>
+            </section>
 
+        </div>
 
-<h2>Information Technology Projects Worked On</h2>
+        <!-- Pricing -->
+        <section class="align--center pt2 pb3">
+            <p class="h2">Rent the whole place for $3,670/night.</p>
+            <p>Maximum of 40 people.</p>
+        </section>
 
-<dl>
-<dt>Fulton County Government Center New Website</dt>
-	<dd>During Internship, assistem team in moving content from old website to new.</dd>
-<dt>GGC Course Registration System</dt>
-	<dd>Designed a more efficient way to register students at GGC.</dd>
+        <!-- Contact -->
+        <section class="bg--secondary-color pt3 pb3">
+            <div class="container">
+                <div class="mb2 align--center">
+                    <h3 class="mb1">Contact us to get started</h3>
+                    <p>We'll reply when we get back from&nbsp;fishing.</p>
+                </div>
+                <form>
+                    <div class="grid-row">
+                        <div class="grid-column span-half mt1">
+                            <label class="label" for="name">Name</label>
+                            <input class="form-control" id="name">
+                        </div>
+                        <div class="grid-column span-half mt1">
+                            <label class="label" for="email">Email</label>
+                            <input class="form-control" id="email" type="email">
+                        </div>
+                    </div>
+                    <label class="label mt1" for="dates">Timeline</label>
+                    <input class="form-control" id="dates">
+                    <label class="label mt1" for="message">Message</label>
+                    <textarea class="form-control" id="message"></textarea>
+                    <div class="align--right mt1">
+                        <button class="btn btn--outline">Send</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+        
+    </main>
 
-<h2>ITEC Skills</h2>
-<ul>
-<li>Java Programming</li>
-<li> HTML</li>
-<li>CSS</li>
-<li>Microsoft Suite Efficient</li>
-<li>Ability to type fast</li>
-
-
-</ul>
-
-</dl>
-
-
-
-
-
-
-
-</main>
-
-
-
-                <?php include 'footer.php';?>
-
-
-
-                <script src="js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- Footer -->
+    <?php include 'footer.php';?>
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
         crossorigin="anonymous"></script>
     <script src="js/vendor/bootstrap.min.js"></script>
@@ -157,8 +167,7 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
     <script src="js/gmaps.min.js"></script>
     <script src="js/main.js"></script>
-</div>
 
-</body>
-
+    </body>
 </html>
+
