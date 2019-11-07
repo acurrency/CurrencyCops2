8863e796713f99d00f217e2aafe55886c53e5a02
Taylor Murphy <Taylor.Murphy@cox.com>
2019-11-06 22:45:18 -0500

    Update public_functions.php

--- includes/public_functions.php
+++ includes/public_functions.php
@@ -3,10 +3,8 @@
 * Returns all published posts
 * * * * * * * * * * * * * * */
 function getPublishedPosts() {
-	// use global $conn object in function
-	global $conn;
 	$sql = "SELECT * FROM posts WHERE published=true";
-	$result = mysqli_query($conn, $sql);
+	$result = $db->query($sql);
 
 	// fetch all posts as an associative array called $posts
 	$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
