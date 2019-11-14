62fa1f99fefe32cbcaa5b10dcea9e9a5d276048b
Taylor Murphy <Taylor.Murphy@cox.com>
2019-11-13 20:57:23 -0500

    Update linkup.php

--- linkup.php
+++ linkup.php
@@ -1,54 +1,55 @@
 <!-- The first include should be config.php -->
 <?php require_once 'dbConfig.php'; ?>
-<?php require_once('/includes/head_section.php'); ?>
-<?php require_once('/includes/public_functions.php'); ?>
-<?php require_once('/includes/registration_login.php'); ?>
+<?php require_once '/includes/head_section.php'; ?>
+<?php require_once ' /includes/public_functions.php'; ?>
+<?php require_once '/includes/registration_login.php'; ?>
 <!-- Retrieve all posts from database  -->
 <?php $posts = getPublishedPosts(); ?>
-<head>
-<title>Link-Up CurrencyCops</title>
-</head>
-<body>
-	<!-- container - wraps whole page -->
-	<div class="container">
-		<!-- navbar -->
-		<?php include('/includes/navbar.php'); ?>
-		<!-- // navbar -->
+<html>
+	<head>
+		<title>Link-Up CurrencyCops</title>
+	</head>
+	<body>
+		<!-- container - wraps whole page -->
+		<div class="container">
+			<!-- navbar -->
+			<?php include '/includes/navbar.php'; ?>
+			<!-- // navbar -->
 
-		<!-- banner -->
-		<?php include('/includes/banner.php'); ?>
-		<!-- // banner -->
+			<!-- banner -->
+			<?php include '/includes/banner.php'; ?>
+			<!-- // banner -->
 
-		<!-- Page content -->
-		<div class="content">
-				<h2 class="content-title">Recent Articles</h2>
-				<hr>
-				<!-- more content still to come here ... -->
-				
-				<!-- Add this ... -->
-			<?php foreach ($posts as $post) { ?>
-				<div class="post" style="margin-left: 0px;">
-					<img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
-					<?php if (isset($post['topic']['name'])) { ?>
-					<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
-						<div class="post_info">
-							<h3><?php echo $post['title']; ?></h3>
-							<div class="info">
-								<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
-								<span class="read_more">Read more...</span>
+			<!-- Page content -->
+			<div class="content">
+					<h2 class="content-title">Recent Articles</h2>
+					<hr>
+					<!-- more content still to come here ... -->
+					
+					<!-- Add this ... -->
+				<?php foreach ($posts as $post) { ?>
+					<div class="post" style="margin-left: 0px;">
+						<img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+						<?php if (isset($post['topic']['name'])) { ?>
+						<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+							<div class="post_info">
+								<h3><?php echo $post['title']; ?></h3>
+								<div class="info">
+									<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
+									<span class="read_more">Read more...</span>
+								</div>
 							</div>
-						</div>
-					</a>
-					<?php } ?>
-				</div>
-			<?php } ?>
+						</a>
+						<?php } ?>
+					</div>
+				<?php } ?>
+			</div>
 		</div>
-	</div>
-
 		<!--</div> -->
-		<!-- // Page content -->
+			<!-- // Page content -->
 
-		<!-- footer -->
-		<?php include('/includes/footer.php'); ?>
-		<!-- // footer -->
-</body>
+			<!-- footer -->
+		<?php include '/includes/footer.php'; ?>
+			<!-- // footer -->
+	</body>
+</html>
