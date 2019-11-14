72e0bda7afef7b6fab8d4a7d21cf642db57a08eb
Taylor Murphy <Taylor.Murphy@cox.com>
2019-11-13 22:02:13 -0500

    c

--- includes/public_functions.php
+++ includes/public_functions.php
@@ -7,8 +7,8 @@ require_once 'dbConfig.php';
 function getPublishedPosts() {
 	// use global $conn object in function
 	global $conn;
-	$sql = "SELECT * FROM posts WHERE published=true";
-	$result = mysqli_query($db, $sql);
+	$result = $db->query("SELECT * FROM posts WHERE published=true");
+	//$result = mysqli_query($db, $sql);
 	// fetch all posts as an associative array called $posts
 	$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
--- linkup.php
+++ linkup.php
@@ -1,7 +1,7 @@
-<?php include 'config.php'; ?>
-<?php require_once( ROOT_PATH . '/includes/head_section.php'); ?>
+<?php require_once('config.php'); ?>
 <?php require_once( ROOT_PATH . '/includes/public_functions.php'); ?>
 <?php require_once( ROOT_PATH . '/includes/registration_login.php'); ?>
+<?php require_once( ROOT_PATH . '/includes/head_section.php'); ?>
 <?php $posts = getPublishedPosts(); ?>
 
 	<title>Link-Up CurrencyCops</title>
